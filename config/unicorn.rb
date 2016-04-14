561595fe51b127a17653c7ededdf4e1d4ebcaa74
fushang318 <fushang318@gmail.com>
2016-04-14 12:55:53 +0800

    add deploy sh

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-worker_processes 1 
+worker_processes 3
 preload_app true
 timeout 60
 
@@ -7,5 +7,5 @@ app_path = File.expand_path("../../", __FILE__)
 listen "#{app_path}/tmp/sockets/unicorn.sock", :backlog => 2048
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
-stderr_path("#{app_path}/tmp/logs/unicorn-error.log")
-stdout_path("#{app_path}/tmp/logs/unicorn.log")
+stderr_path("#{app_path}/log/unicorn-error.log")
+stdout_path("#{app_path}/log/unicorn.log")
